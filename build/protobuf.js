27bac85a0d8dfb458be2d5caea3917d2e1deca53
Kasra Karami <Kasra_K2K@yahoo.com>
2023-09-25 10:40:22 +0330

    add build into package

---
+++ build/protobuf.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authGrpcObj = exports.loaderOptions = exports.protoLoader = exports.grpc = void 0;
+/* ------------------------------ Dependencies ------------------------------ */
+exports.grpc = require('@grpc/grpc-js');
+exports.protoLoader = require('@grpc/proto-loader');
+/* ------------------------------ Node Modules ------------------------------ */
+const node_path_1 = require("node:path");
+/* -------------------------------- Constants ------------------------------- */
+exports.loaderOptions = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true, // sets virtual oneof properties to field names
+};
+/* ---------------------------------- Auth ---------------------------------- */
+const AUTH_PROTO_PATH = (0, node_path_1.resolve)(process.cwd(), './proto/auth.proto');
+const authPackageDef = exports.protoLoader.loadSync(AUTH_PROTO_PATH, exports.loaderOptions);
+exports.authGrpcObj = exports.grpc.loadPackageDefinition(authPackageDef);
---
+++ build/services.d.ts
@@ -0,0 +1,13 @@
+export declare enum ServiceName {
+    DEFAULT = 0,
+    MAIN = 1,
+    BUSINNESS = 2,
+    ASSET = 3,
+    GATEWAY = 4
+}
+export declare const BaseUrl: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+};
