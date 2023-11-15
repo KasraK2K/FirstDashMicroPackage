507ecd7c30805fcc4d788580192d4b3b899a9014
Kasra Karami <Kasra.Karami.Work@gmail.com>
2023-11-15 10:35:14 +0330

    prettier some part of code

--- src/services.ts
+++ src/services.ts
@@ -23,9 +23,9 @@ export enum ServiceName {
 /* -------------------------------------------------------------------------- */
 // prettier-ignore
 export const BaseUrl = {
-  [ServiceName.DEFAULT]:					`${process.env.SERVER_ADDRESS}/api/v1`,
-  [ServiceName.MAIN]:							`${process.env.MAIN_SERVER_ADDRESS}/api/v1`,
-  [ServiceName.BUSINESS]:					`${process.env.BUSINESS_SERVER_ADDRESS}/api/v1`,
-  [ServiceName.ASSET]:						`${process.env.ASSET_SERVER_ADDRESS}/api/v1`,
+  [ServiceName.DEFAULT]:			`${process.env.SERVER_ADDRESS}/api/v1`,
+  [ServiceName.MAIN]:				`${process.env.MAIN_SERVER_ADDRESS}/api/v1`,
+  [ServiceName.BUSINESS]:			`${process.env.BUSINESS_SERVER_ADDRESS}/api/v1`,
+  [ServiceName.ASSET]:				`${process.env.ASSET_SERVER_ADDRESS}/api/v1`,
   [ServiceName.STAGING_BUSINESS]:	`${process.env.STAGING_BUSINESS_SERVER_ADDRESS}/api/v1`,
 }
